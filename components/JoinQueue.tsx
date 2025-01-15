07557586fc54ddcc867147b0f8ff2aeff6c5fe47
Aditya <4adi26@gmail.com>
2025-01-15 22:29:33 +0530

    Add event page

---
+++ components/JoinQueue.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Id } from "@/convex/_generated/dataModel"
+
+const JoinQueue = ({ eventId, userId }: {eventId: Id<"events">, userId: Id<"users">}) => {
+
+  return (
+    <div>JoinQueue</div>
+  )
+}
+
+export default JoinQueue
