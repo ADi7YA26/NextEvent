2ddd00a7009263e050cb9e4bd2404b91280b3a2c
Aditya <4adi26@gmail.com>
2025-01-18 22:50:58 +0530

    Added dark mode toggler

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,19 @@ import Link from "next/link"
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import SearchBar from "./SearchBar"
 import { Button } from "./ui/button"
+import { ModeToggle } from "./ModeToggle"
 
 const Header = () => {
 	return (
-		<div className="border-b px-5">
-			<div className="flex flex-col lg:flex-row items-center gap-4 p-4">
+		<header className="border-b z-10 sticky top-0 sm:px-5 backdrop-blur-md">
+			<nav className="flex flex-col lg:flex-row items-center gap-4 p-4">
 				<div className="flex items-center justify-between w-full lg:w-auto">
 					<Link href="/" className="font-bold shrink-0 mr-5">
-            <h1 className="text-2xl">Next<span className="text-primary text-blue-600">Event</span></h1>
+            <h1 className="text-2xl">Next<span className="text-primary">Ticket</span></h1>
 					</Link>
 
-					<div className="lg:hidden">
+					<div className="flex items-center gap-2 lg:hidden">
+            <ModeToggle />
 						<SignedIn>
 							<UserButton />
 						</SignedIn>
@@ -33,7 +35,8 @@ const Header = () => {
 				</div>
 
 
-        <div className="hidden lg:block ml-auto">
+        <div className="hidden lg:flex gap-2 ml-auto">
+          <ModeToggle />
           <SignedIn>
             <div className="flex items-center gap-3">
               <Link href="/seller">
@@ -63,22 +66,22 @@ const Header = () => {
 				{/* Mobile Action Buttons */}
         <div className="lg:hidden w-full flex justify-center gap-3">
           <SignedIn>
-            <Link href="/seller" className="flex-1">
-              <Button>
+            <Button asChild>
+              <Link href="/seller" className="flex-1">
                 Sell Tickets
-              </Button>
-            </Link>
+              </Link>
+            </Button>
 
-            <Link href="/tickets" className="flex-1">
-              <Button variant="secondary">
+            <Button variant="secondary" asChild>
+              <Link href="/tickets" className="flex-1">
                 My Tickets
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           </SignedIn>
         </div>
 
-			</div>
-		</div>
+			</nav>
+		</header>
 	)
 }
 
